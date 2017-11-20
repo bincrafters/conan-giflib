f6edf4fccdf48bd41a7717e329f586fd1dcbb7f8
Jerry W <jerrywiltse@gmail.com>
2017-11-19 22:59:17 -0500

    working windows refactoring

--- conanfile.py
+++ conanfile.py
@@ -9,12 +9,11 @@ from conans import CMake, AutoToolsBuildEnvironment, ConanFile, tools
 class ZlibNgConan(ConanFile):
     name = "giflib"
     version = "5.1.3"
-    ZIP_FOLDER_NAME = "giflib-%s" % version 
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = "shared=False", "fPIC=True"
-    url = "http://github.com/ZaMaZaN4iK/conan-giflib"
+    url = "http://github.com/bincrafters/conan-giflib"
     license = "https://sourceforge.net/p/giflib/code/ci/master/tree/COPYING"
     exports = ["FindGIF.cmake", "CMakeLists.txt", "getopt.c", "getopt.h", "unistd.h.in"]
     install = 'gitfil-install'
@@ -33,13 +32,24 @@ class ZlibNgConan(ConanFile):
             # self.ZIP_FOLDER_NAME = "giflib-%s-windows" % self.version
 
     def source(self):
-        zip_name = "%s.tar.gz" % self.ZIP_FOLDER_NAME
-        tools.download("http://downloads.sourceforge.net/project/giflib/%s" % zip_name, zip_name)
-        tools.unzip(zip_name)
-        os.unlink(zip_name)
-        if not self.settings.os != "Windows":
+        zip_name = "%s-%s" % (self.name, self.version)
+        tools.get("http://downloads.sourceforge.net/project/giflib/%s.tar.gz" % zip_name)
+        if self.settings.os == "Windows":
             for filename in ["CMakeLists.txt", "getopt.c", "getopt.h", "unistd.h.in"]:
-                shutil.copy(filename, os.path.join(self.ZIP_FOLDER_NAME, filename))
+                shutil.copy(filename, os.path.join(zip_name, filename))
+        os.rename(zip_name, "sources")
+
+    def build(self):
+        if self.settings.compiler == "Visual Studio":
+            self.build_windows()
+        else:
+            self.build_configure()
+
+    def build_windows(self):
+        cmake = CMake(self)
+        cmake.configure(source_dir="sources")
+        self.output.info('Running CMake command: ' + cmake.command_line)
+        cmake.build()
 
     def build_configure(self):
         prefix = os.path.abspath(self.install)
@@ -52,7 +62,7 @@ class ZlibNgConan(ConanFile):
         else:
             args.extend(['--enable-static', '--disable-shared'])
 
-        with tools.chdir(self.ZIP_FOLDER_NAME):
+        with tools.chdir("sources"):
             if self.settings.os == "Macos":
                 old_str = r'-install_name \$rpath/\$soname'
                 new_str = r'-install_name \$soname'
@@ -63,43 +73,20 @@ class ZlibNgConan(ConanFile):
             env_build.make()
             env_build.make(args=['install'])
 
-    def build_windows(self):
-        cmake = CMake(self.settings)
-        self.run("cd %s && mkdir _build" % self.ZIP_FOLDER_NAME)
-        cd_build = "cd %s/_build" % self.ZIP_FOLDER_NAME
-        self.output.warn('%s && cmake .. %s' % (cd_build, cmake.command_line))
-        self.run('%s && cmake .. %s' % (cd_build, cmake.command_line))
-        self.output.warn("%s && cmake --build . %s" % (cd_build, cmake.build_config))
-        self.run("%s && cmake --build . %s" % (cd_build, cmake.build_config))
-
-    def build(self):
-        if self.settings.os == "Windows":
-            self.build_windows()
-        else:
-            self.build_configure()
-
+            
     def package(self):
         # Copy FindGIF.cmake to package
         self.copy("FindGIF.cmake", ".", ".")
         
         # Copying zlib.h, zutil.h, zconf.h
-        self.copy("*.h", "include", "%s" % self.ZIP_FOLDER_NAME, keep_path=False)
-        self.copy("*.h", "include", "%s" % "_build", keep_path=False)
-
-        if not self.settings.os == "Windows" and self.options.shared:
-            if self.settings.os == "Macos":
-                self.copy(pattern="*.dylib", dst="lib", keep_path=False)
-                self.copy(pattern="**.a", dst="lib", src=self.ZIP_FOLDER_NAME, keep_path=False)
-            else:
-                self.copy(pattern="*.so*", dst="lib", src=self.ZIP_FOLDER_NAME, keep_path=False)
-                self.copy(pattern="**.a", dst="lib", src=self.ZIP_FOLDER_NAME, keep_path=False)
-        else:
-            self.copy(pattern="*.a", dst="lib", src="%s/_build" % self.ZIP_FOLDER_NAME, keep_path=False)
-            self.copy(pattern="*.a", dst="lib", src=self.ZIP_FOLDER_NAME, keep_path=False)
-            self.copy(pattern="*.lib", dst="lib", src=self.ZIP_FOLDER_NAME, keep_path=False)
-
+        self.copy("*.h", "include", "%s" % "sources", keep_path=False)
+        self.copy(pattern="libgif.lib", dst="lib", keep_path=False)
+        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+        self.copy(pattern="*.a", dst="lib", keep_path=False)
+        self.copy(pattern="*.so*", dst="lib", keep_path=False)
+        
     def package_info(self):
-        if self.settings.os == "Windows":
+        if self.settings.compiler == "Visual Studio":
             if self.settings.build_type == "Debug":
                 self.cpp_info.libs = ['libgifd', 'getargd']
             else:
